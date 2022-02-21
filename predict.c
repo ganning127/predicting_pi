ade8303c97c93cb6b44380e929977400aec5e2e0
Ganning Xu <ganningxu@gmail.com>
2022-02-21 16:42:47 -0500

    first commit

---
+++ predict.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+double get_pi(int darts);
+float rand_float(float min, float max);
+
+int main(void)
+{
+    const double MATH_PI = 3.14159265358979323846;
+    int darts = 1;
+    int decimals = 0;
+    int wrong;
+    double pred_pi;
+    srand(time(0));
+
+    while (decimals <= 5)
+    {
+        wrong = 0;
+        for (int i = 0; i < 100; i++)
+        {
+            pred_pi = get_pi(darts);
+            // printf("pred_pi: %f, pi: %f\n", (pred_pi * pow(10, decimals), (MATH_PI * pow(10, decimals))));
+            // printf("pred_pi: %d, pi: %d\n", (int)(pred_pi * pow(10, decimals)), (int)(MATH_PI * pow(10, decimals)));
+            if ((int)(pred_pi * pow(10, decimals)) != (int)(MATH_PI * pow(10, decimals)))
+            {
+                wrong += 1;
+            }
+            if (wrong > 5)
+            {
+                darts *= 10;
+                // printf("%d\n", darts);
+                break;
+            }
+        }
+        if (wrong <= 5)
+        {
+            printf("Darts: %d || Decimal places: %d || Example PI: %f\n", darts, decimals, pred_pi);
+            decimals += 1;
+        }
+    }
+
+    puts("");
+
+    return 0;
+}
+
+double get_pi(int darts)
+{
+    int hits = 0;
+
+    for (int i = 0; i < darts; i++)
+    {
+        double x = (double)rand_float(-1, 1);
+        double y = (double)rand_float(-1, 1);
+
+        if (x * x + y * y <= 1)
+        {
+            hits++;
+        }
+    }
+
+    return 4.0 * (double)hits / (double)darts;
+}
+
+float rand_float(float min, float max)
+{
+    return ((max - min) * ((float)rand() / RAND_MAX)) + min;
+}
